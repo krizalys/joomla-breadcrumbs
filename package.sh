1ecc0a752a2ff6ec0ae8a5b4f4c00d2f583be028
Christophe Vidal <kriss@krizalys.com>
2013-05-26 20:01:43 +0700

    See ChangeLog.txt

---
+++ package.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+VERSION="1.0.2"
+
+cd joomla/mod_krizalys_breadcrumbs; zip -r ../../mod_krizalys_breadcrumbs-${VERSION}-all-languages.zip .; cd ../..
+cp mod_krizalys_breadcrumbs-${VERSION}-all-languages.zip mod_krizalys_breadcrumbs-${VERSION}.zip
+
+for locale in fr-FR it-IT; do
+	zip -d mod_krizalys_breadcrumbs-${VERSION}.zip \
+		language/${locale}/* \
+		language/${locale}/
+
+	xml="mod_krizalys_breadcrumbs-${locale}.xml"
+	dir="mod_krizalys_breadcrumbs-${VERSION}-${locale}"
+	mkdir -p ${dir}/language/${locale}
+	cp -R joomla/mod_krizalys_breadcrumbs/language/${locale}/* ${dir}/language/${locale}
+	cp joomla/mod_krizalys_breadcrumbs/index.html ${dir}/language
+	cp joomla/mod_krizalys_breadcrumbs/index.html ${dir}
+	cp joomla/${xml} ${dir}
+	cd ${dir}; zip -r ../${dir}.zip .; cd ..
+	rm -rf ${dir}
+done
