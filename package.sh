59e4c58ebf8340d269d653a3609fee5321c02dab
Christophe Vidal <kriss@krizalys.com>
2015-06-13 11:45:26 +0700

    Indenting using spaces

--- package.sh
+++ package.sh
@@ -6,17 +6,17 @@ cd joomla/mod_krizalys_breadcrumbs; zip -r ../../mod_krizalys_breadcrumbs-${VERS
 cp mod_krizalys_breadcrumbs-${VERSION}-all-languages.zip mod_krizalys_breadcrumbs-${VERSION}.zip
 
 for locale in de-DE es-ES fr-FR it-IT nl-NL; do
-	zip -d mod_krizalys_breadcrumbs-${VERSION}.zip \
-		language/${locale}/* \
-		language/${locale}/
+    zip -d mod_krizalys_breadcrumbs-${VERSION}.zip \
+        language/${locale}/* \
+        language/${locale}/
 
-	xml="mod_krizalys_breadcrumbs-${locale}.xml"
-	dir="mod_krizalys_breadcrumbs-${VERSION}-${locale}"
-	mkdir -p ${dir}/language/${locale}
-	cp -R joomla/mod_krizalys_breadcrumbs/language/${locale}/* ${dir}/language/${locale}
-	cp joomla/mod_krizalys_breadcrumbs/index.html ${dir}/language
-	cp joomla/mod_krizalys_breadcrumbs/index.html ${dir}
-	cp joomla/${xml} ${dir}
-	cd ${dir}; zip -r ../${dir}.zip .; cd ..
-	rm -rf ${dir}
+    xml="mod_krizalys_breadcrumbs-${locale}.xml"
+    dir="mod_krizalys_breadcrumbs-${VERSION}-${locale}"
+    mkdir -p ${dir}/language/${locale}
+    cp -R joomla/mod_krizalys_breadcrumbs/language/${locale}/* ${dir}/language/${locale}
+    cp joomla/mod_krizalys_breadcrumbs/index.html ${dir}/language
+    cp joomla/mod_krizalys_breadcrumbs/index.html ${dir}
+    cp joomla/${xml} ${dir}
+    cd ${dir}; zip -r ../${dir}.zip .; cd ..
+    rm -rf ${dir}
 done
